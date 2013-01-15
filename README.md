86710909c170e3b3516521ff228babd9dca3ff68
Winter Mason <winteram@gmail.com>
2013-01-15 16:44:17 -0500

    Create folder structure

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ BIA660-2013S
 ============
 
 Repository for Web Analytics (BIA 660), Spring 2013
+
+This repository holds:
+
+- example code
+- slides from class
+- course documents
+- descriptions of assignments
+
+This will also be where you submit your work!
---
+++ example_code/factorial.py
@@ -0,0 +1,17 @@
+def myfactorial(n):
+    """Expects an integer as input
+    Returns the factorial of a number.
+    """
+    myfact = 1
+    for i in range(n):
+        myfact = myfact * (n - i)
+    return myfact
+
+def mychoose(n,k):
+    """Returns the number of permutations for k draws from n items"""
+    return myfactorial(n) / (myfactorial(k) * myfactorial(n - k))
+
+if __name__ == "__main__":
+    
+    print "The factorial for 6 = %d" % myfactorial(6)
+    print "The number of permutations for 3 items chosen from a set of 10 = %d" % mychoose(10,3)
