bc48831cb2e9d2888b08df8f4df6744aeae01977
Nengjun Zhao <nzhao@stevens.edu>
2013-01-29 15:24:22 -0800

    assignment1-Nengjun Zhao

---
+++ course_docs/20130122/Assignment1-Nengjun Zhao.py
@@ -0,0 +1,43 @@
+def jiami(s,key):
+    b=dytearray(str(s).encode('gbk'))
+    n=len(b)
+    c=bytearray(n*2)
+    j=0
+    m=len(c)
+    for i in range(0,n):
+        b1=b[i]
+        b2=b1^key
+        c1=b2%16
+        c2=b2//16
+        c1=c1+65
+        c2=c2+65
+        i=i+1
+        for j in range(0,m):
+            c[j]=c1
+            c[j+1]=c2
+            j=j+2
+return c.decode("gbk")
+
+def jiemi( s, key ):
+c = bytearray( str(s).encode("gbk") )
+n = len(c)
+if n % 2 != 0 :
+return ""
+n = n // 2
+b = bytearray( n )
+j = 0
+for j in range( 0, n ):
+    c1 = c[j] 
+    c2 = c[j+1] 
+    j = j+2
+    c1 = c1 - 65
+    c2 = c2 - 65
+    b2 = c2*16 + c1
+    b1 = b2^ key
+    for i in range(0,n)    
+        b[i]= b1
+        i=i+1
+try: 
+   return b.decode("gbk")
+except:
+return "lfkjdsf"
