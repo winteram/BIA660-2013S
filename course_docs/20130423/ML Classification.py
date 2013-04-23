442c53729272af8cb5bb0eb2bfc3072ad44d6f4f
Winter Mason <winteram@gmail.com>
2013-04-23 15:27:27 -0400

    Added docs for today's class

---
+++ course_docs/20130423/ML Classification.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import numpy as np
+
+# <codecell>
+
+import pylab as pl
+
+# <codecell>
+
+import scipy
+
+# <codecell>
+
+import sklearn as sk
+
+# <codecell>
+
+# import some data to play with
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+Y = iris.target
+
+# <codecell>
+
+h = .02  # step size in the mesh
+
+logreg = linear_model.LogisticRegression(C=1e5)
+
+# we create an instance of Neighbours Classifier and fit the data.
+logreg.fit(X, Y)
+
+# <codecell>
+
+# Plot the decision boundary. For that, we will asign a color to each
+# point in the mesh [x_min, m_max]x[y_min, y_max].
+x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+Z = logreg.predict(np.c_[xx.ravel(), yy.ravel()])
+
+# Put the result into a color plot
+Z = Z.reshape(xx.shape)
+pl.figure(1, figsize=(4, 3))
+pl.pcolormesh(xx, yy, Z, cmap=pl.cm.Paired)
+
+# Plot also the training points
+pl.scatter(X[:, 0], X[:, 1], c=Y, edgecolors='k', cmap=pl.cm.Paired)
+pl.xlabel('Sepal length')
+pl.ylabel('Sepal width')
+
+pl.xlim(xx.min(), xx.max())
+pl.ylim(yy.min(), yy.max())
+pl.xticks(())
+pl.yticks(())
+
+pl.show()
+
